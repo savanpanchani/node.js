9941c8222b23401ef88d483b98e46c57386c670b
savan panchani <savanpanchani18@gmail.com>
2025-10-13 16:56:17 +0530

    pr-5

--- pr-5/config/dbconnection.js
+++ pr-5/config/dbconnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbconnection = () => {
-    mongoose.connect("mongodb://localhost:27017/")
+    mongoose.connect("mongodb+srv://savanpanchani18_db_user:Savan0018@cluster0.9pjdcr1.mongodb.net/movie")
         .then(() => console.log("DB is connected..."))
         .catch(err => console.error("DB Connection Error:", err));
 };
